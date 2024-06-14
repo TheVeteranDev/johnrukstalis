9d86ffd602d329228f40434bd7bc817f2f72d590
John Rukstalis <theveterandev@gmail.com>
2024-06-14 16:54:51 -0400

    reverted resource.ts in storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,10 +1,5 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-    name: 'johnrukstalis_storage',
-    access: (allow) => ({
-        "*": [
-            allow.guest.to(["read"])
-        ]
-    })
+    name: 'johnrukstalis_storage'
 });
