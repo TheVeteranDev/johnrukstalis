7132293d8e2a020d46969df3aea483a15657a0a0
John Rukstalis <theveterandev@gmail.com>
2024-06-15 11:57:00 -0400

    removed linting errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,45 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-import { downloadData } from "aws-amplify/storage";
+// import { useEffect, useState } from "react";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
+// import { downloadData } from "aws-amplify/storage";
 import { Canvas } from "@react-three/fiber";
-import * as THREE from "three";
+// import * as THREE from "three";
 import { OrbitControls, Stars } from "@react-three/drei";
 import Sphere from "./components/sphere/Sphere";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const [earthTexture, setEarthTexture] = useState<THREE.Texture>();
+  // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  // const [earthTexture, setEarthTexture] = useState<THREE.Texture>();
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+  // useEffect(() => {
+  //   client.models.Todo.observeQuery().subscribe({
+  //     next: (data) => setTodos([...data.items]),
+  //   });
 
-    getEarthTexture();
-  }, []);
+  //   getEarthTexture();
+  // }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  // function createTodo() {
+  //   client.models.Todo.create({ content: window.prompt("Todo content") });
+  // }
 
-  const deleteTodo = (id: string) => {
-    client.models.Todo.delete({ id });
-  }
+  // const deleteTodo = (id: string) => {
+  //   client.models.Todo.delete({ id });
+  // }
 
-  const getEarthTexture = async () => {
-    const result2 = await downloadData({
-      path: "assets/earth8k.jpeg",
-    }).result;
+  // const getEarthTexture = async () => {
+  //   const result2 = await downloadData({
+  //     path: "assets/earth8k.jpeg",
+  //   }).result;
 
-    const url = URL.createObjectURL(await result2.body.blob());
-    const texture = new THREE.TextureLoader().load(result2.path);
-    setEarthTexture(texture);
+  //   const url = URL.createObjectURL(await result2.body.blob());
+  //   const texture = new THREE.TextureLoader().load(result2.path);
+  //   setEarthTexture(texture);
 
-    return url;
-  }
+  //   return url;
+  // }
 
   return (
     <main className="container-fluid">
